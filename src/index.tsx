52994db4540f528382af8c779b5515905a60d7cf
sergnio <sergnioa@gmail.com>
2021-06-13 12:23:06 -0500

    got SW reg from new CRA app

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
+import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
 import App from "./App";
-import * as serviceWorker from "./serviceWorker";
 
 // if (process.env.NODE_ENV === 'development') {
 //     const { worker } = require('./mocks/browser')
@@ -12,6 +12,6 @@ import * as serviceWorker from "./serviceWorker";
 ReactDOM.render(<App />, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
-// unregister() to register() below. NoteDAO this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.register();
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://cra.link/PWA
+serviceWorkerRegistration.register();
