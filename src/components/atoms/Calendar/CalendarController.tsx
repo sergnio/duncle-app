e8de9febaa4fd02b5e1590e0060d6fcddb45fa44
sergnio <sergnioa@gmail.com>
2021-06-13 11:17:36 -0500

    Almost works

--- src/components/atoms/Calendar/CalendarController.tsx
+++ src/components/atoms/Calendar/CalendarController.tsx
@@ -4,19 +4,20 @@ import event, { TrimmedEvent } from "../../../model/event";
 import useAuth from "../../../common/hooks/Auth/useAuth";
 import useUsersQuery from "../../../common/queries/useUsersQuery";
 import { useSeeOthersState } from "../../../common/providers/SeeOthersProvider";
+import { PouchResponse } from "../../../common/queries/queriesUtils";
+import UserDAO from "../../../model/userDAO";
 
-const todayStr = new Date().toISOString().replace(/T.*$/, ""); // YYYY-MM-DD of today
-let eventGuid = 0;
-
-export function createEventId() {
-  return String(eventGuid++);
-}
+const getUserEvents = (
+  user: "jim" | "sam" | string,
+  userData: PouchResponse<UserDAO>
+) => userData.rows.filter((d) => d.doc.username === user)[0].doc.events;
 
 export default () => {
   const INITIAL_EVENT_STATE: TrimmedEvent[] = [];
   const [events, setEvents] = useState<TrimmedEvent[]>(INITIAL_EVENT_STATE);
+  console.log("wtf are these events rn", events);
   const { getAuthenticatedUser } = useAuth();
-  // const { data } = useUsersQuery();
+  const { data } = useUsersQuery();
   const { checked } = useSeeOthersState();
 
   // todo - why does this need to be in a useEffect?
@@ -26,31 +27,27 @@ export default () => {
     // eslint-disable-next-line
     let allEvents: TrimmedEvent[] = [];
 
-    const filteredEvents: TrimmedEvent[] | undefined = user?.events.map(
-      ({ id, title, start, end }: event) => ({ id, title, start, end })
-    );
-
-    if (filteredEvents != null) {
-      allEvents = [...allEvents, ...filteredEvents];
+    if (data && checked.checkedTerry && user?.username != null) {
+      console.log("checked terry");
+      const currentUserEvents = getUserEvents(user.username, data);
+      allEvents = [...allEvents, ...currentUserEvents];
     } else {
       console.log("No events were present");
     }
 
-    console.log("triggering");
     if (checked.checkedSam) {
-      console.log("checked sam");
-      const nw = [
-        {
-          id: createEventId(),
-          title: "All-day event",
-          start: todayStr,
-        },
-      ];
-      allEvents = [...allEvents, ...nw];
+      const samEvents = getUserEvents("sam", data);
+      allEvents = [...allEvents, ...samEvents];
+    }
+
+    if (checked.checkedJim) {
+      const jimEvents = getUserEvents("jim", data);
+      console.log({ jimEvents });
+      allEvents = [...allEvents, ...jimEvents];
     }
 
     setEvents(allEvents);
-  }, [checked]);
+  }, [checked, data]);
 
   return (
     <>
