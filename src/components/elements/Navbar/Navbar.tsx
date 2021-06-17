ddb746ebdf94d329fda602e86de7b90191e65404
sergnio <sergnioa@gmail.com>
2021-06-17 17:27:54 -0500

    Fix navbar

--- src/components/elements/Navbar/Navbar.tsx
+++ src/components/elements/Navbar/Navbar.tsx
@@ -1,6 +1,7 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { AppBar, Tab, Tabs } from "@material-ui/core";
-import { useHistory } from "react-router-dom";
+import { Location } from "history";
+import { useHistory, useLocation } from "react-router-dom";
 import Profile from "../Profile/Profile";
 
 type NavbarProps = {
@@ -8,18 +9,29 @@ type NavbarProps = {
   route: string;
 };
 
-export default function Navbar() {
-  const navbarTabs: NavbarProps[] = [
-    { name: "Dashboard", route: "/dashboard" },
-    { name: "Calendar", route: "/calendar" },
-    { name: "Add Library", route: "/library/new" },
-  ];
+const navbarTabs: NavbarProps[] = [
+  { name: "Dashboard", route: "/dashboard" },
+  { name: "Calendar", route: "/calendar" },
+  { name: "Add Library", route: "/library/new" },
+];
+const DEFAULT_NAVBAR = 0;
 
-  const [currentTab, setCurrentTab] = useState(0);
+export default function Navbar() {
+  const { pathname } = useLocation<Location>();
+
+  const [currentTab, setCurrentTab] = useState(DEFAULT_NAVBAR);
   const history = useHistory();
 
-  const handleChange = (event: any, newValue: number) => {
-    setCurrentTab(newValue);
+  useEffect(() => {
+    const matchingIndex = navbarTabs.findIndex(
+      ({ route }) => route === pathname
+    );
+    const indexNotFound = matchingIndex === -1;
+
+    setCurrentTab(indexNotFound ? DEFAULT_NAVBAR : matchingIndex);
+  }, [setCurrentTab, pathname]);
+
+  const handleChange = (_: any, newValue: number) => {
     history.push(navbarTabs[newValue].route);
   };
 
