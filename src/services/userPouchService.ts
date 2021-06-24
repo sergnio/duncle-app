40bbccd4376a4e5a8f3ed5ff2b3645b14bc5f819
sergnio <sergnioa@gmail.com>
2021-06-23 20:40:53 -0500

    return string of all users

--- src/services/userPouchService.ts
+++ src/services/userPouchService.ts
@@ -1,17 +1,20 @@
 import { createDatabaseWithUser } from "../common/hooks/UsePouch";
+import PouchDB from "pouchdb";
+import UserDAO from "../model/userDAO";
 
 /**
  * Don't use this outside of usePouch or userPouchService
  */
 export const createUserDatabase = () => createDatabaseWithUser("user");
 
-export const getAllUserDatabases = async () => {
-  const localPouch = createUserDatabase();
-  const x = await localPouch.allDocs();
-  console.log({ x });
+export const getAllUserDatabases = async (): Promise<string[]> => {
   /*
         1. Loop through the users DB
         2. Check each of them for a !404(?)
         3. Filter out any without the _test suffix
        */
+  const localPouch = createUserDatabase();
+  const { rows }: PouchDB.Core.AllDocsResponse<UserDAO> =
+    await localPouch.allDocs();
+  return rows.map(({ id }) => id);
 };
