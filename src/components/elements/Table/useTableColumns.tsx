cbb48f918d79a561245f27359e08d6df97967251
sergnio <sergnioa@gmail.com>
2021-07-04 14:13:08 -0500

    Added territories to dashboard

--- src/components/elements/Table/useTableColumns.tsx
+++ src/components/elements/Table/useTableColumns.tsx
@@ -4,65 +4,83 @@ import { Library } from "../../../model";
 import StackedField from "../../atoms/Table/StackedField";
 import { readableDate } from "../../../utils/dateUtil";
 import { formatContactType } from "../../../utils/textFormatUtils";
+import useTerritoriesQuery from "../../../queries/useTerritoriesQuery";
 
-export default (): Column<Library>[] => [
-  { title: "Library", field: "libraryName" },
-  { title: "County", field: "county" },
-  {
-    title: "Contact / Aide",
-    field: "contactName",
-    render: ({ librarian, assistant }: Library) => (
-      <StackedField top={librarian} bottom={assistant} />
-    ),
-  },
-  {
-    title: "Phone / Email",
-    field: "contactInfo",
-    render: ({ phoneNumber, email }: Library) => (
-      <StackedField top={phoneNumber} bottom={email} />
-    ),
-  },
-  {
-    title: "Last Sale / Date",
-    field: "lastSaleInfo",
-    render: ({ lastSale, dateLastSale }: Library) => {
-      const formatter = new Intl.NumberFormat("en-US", {
-        style: "currency",
-        currency: "USD",
-        minimumFractionDigits: 2,
-      });
-      const formattedToDollars = formatter.format(lastSale);
-      if (dateLastSale !== undefined) {
-        dateLastSale = readableDate(dateLastSale);
-      }
-      return <StackedField top={formattedToDollars} bottom={dateLastSale} />;
-    },
-  },
-  {
-    title: "Last Contacted",
-    field: "dateLastContact",
-    render: ({ dateLastContact, lastContactType }: Library) => {
-      const lastContactText =
-        dateLastContact !== undefined
-          ? readableDate(dateLastContact)
-          : dateLastContact;
+export default (): Column<Library>[] => {
+  const { data: territories } = useTerritoriesQuery();
 
-      return (
-        <StackedField
-          top={lastContactText}
-          bottom={formatContactType(lastContactType)}
-        />
-      );
+  return [
+    { title: "Library", field: "libraryName" },
+    { title: "County", field: "county" },
+    {
+      title: "Contact / Aide",
+      field: "contactName",
+      render: ({ librarian, assistant }: Library) => (
+        <StackedField top={librarian} bottom={assistant} />
+      ),
     },
-  },
-  {
-    title: "Next Contact",
-    field: "dateNextContact",
-    defaultSort: "asc",
-    render: ({ dateNextContact }: Library) => {
-      return dateNextContact !== undefined
-        ? readableDate(dateNextContact)
-        : dateNextContact;
+    {
+      title: "Territory",
+      field: "territoryId",
+      render: ({ territoryId, libraryName }: Library) => {
+        const defaultName = {
+          name: "No Territory",
+        };
+        const display = territoryId
+          ? territories?.find((t) => t._id === territoryId) ?? defaultName
+          : defaultName;
+        return <p>{display.name}</p>;
+      },
     },
-  },
-];
+    {
+      title: "Phone / Email",
+      field: "contactInfo",
+      render: ({ phoneNumber, email }: Library) => (
+        <StackedField top={phoneNumber} bottom={email} />
+      ),
+    },
+    {
+      title: "Last Sale / Date",
+      field: "lastSaleInfo",
+      render: ({ lastSale, dateLastSale }: Library) => {
+        const formatter = new Intl.NumberFormat("en-US", {
+          style: "currency",
+          currency: "USD",
+          minimumFractionDigits: 2,
+        });
+        const formattedToDollars = formatter.format(lastSale);
+        if (dateLastSale !== undefined) {
+          dateLastSale = readableDate(dateLastSale);
+        }
+        return <StackedField top={formattedToDollars} bottom={dateLastSale} />;
+      },
+    },
+    {
+      title: "Last Contacted",
+      field: "dateLastContact",
+      render: ({ dateLastContact, lastContactType }: Library) => {
+        const lastContactText =
+          dateLastContact !== undefined
+            ? readableDate(dateLastContact)
+            : dateLastContact;
+
+        return (
+          <StackedField
+            top={lastContactText}
+            bottom={formatContactType(lastContactType)}
+          />
+        );
+      },
+    },
+    {
+      title: "Next Contact",
+      field: "dateNextContact",
+      defaultSort: "asc",
+      render: ({ dateNextContact }: Library) => {
+        return dateNextContact !== undefined
+          ? readableDate(dateNextContact)
+          : dateNextContact;
+      },
+    },
+  ];
+};
--- src/model/newLibrary.ts
+++ src/model/newLibrary.ts
@@ -18,6 +18,7 @@ export default interface NewLibrary {
   assignedRep: string;
   totalSales: number;
   lastSale: number;
+  territoryId: string;
   personalNotes?: NoteDAO;
   level?: string;
   size?: number;
